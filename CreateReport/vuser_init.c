5c99bf37559df752ea8f20043c9ce36fc6d6b8c2
nikotin <juicep704@gmail.com>
2025-05-22 18:02:29 +0300

    Create report

--- CreateReport/vuser_init.c
+++ CreateReport/vuser_init.c
@@ -1,4 +1,17 @@
+int randNum;
+char reportName[20];
+char randText[16];
+
 vuser_init()
 {
-	return 0;
+        srand(time(NULL));
+
+        randNum = rand() % 10000;
+        sprintf(reportName, "Nik%d", randNum);
+        randNum = rand() % 10000;
+        sprintf(randText, "report %d", randNum);
+
+        lr_save_string(reportName, "reportName");
+        lr_save_string(randText, "randText");
+        return 0;
 }
